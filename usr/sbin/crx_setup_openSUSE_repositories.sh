ca4898910f9d12f69c197b14d0659c8585d27430
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2024-08-14 17:53:40 +0200

    New openLeap version 15.6. Provide online installation. Do not download the installation media

--- usr/sbin/crx_setup_openSUSE_repositories.sh
+++ usr/sbin/crx_setup_openSUSE_repositories.sh
@@ -4,6 +4,7 @@ USAGE="\n
 crx_setup_openSUSE_repositories.sh  [-v Version] [-ueh ]\n
 \n
 \t\t-V Version\tThe openSUSE version: 15.3.\n
+\t\t-o\t\tDo not synchronize the repositories. Configure online installation.\n
 \t\t-u\t\tSynchronize update repository.\n
 \t\t-e\t\tSynchronize education repository.\n
 \t\t-t\t\tSynchronize terminalserver repository.\n
@@ -11,18 +12,22 @@ crx_setup_openSUSE_repositories.sh  [-v Version] [-ueh ]\n
 \t\t-h\t\tThis page\n
 "
 
-VERSION="15.5"
+VERSION="15.6"
+ONLINE="no"
 UPDATE="no"
 EDUCATION="no"
 TERMINALSERVER="no"
 PACKMAN="no"
 SERVER="rsync.opensuse.org"
+DATE=$( /usr/share/cranix/tools/crx_date.sh )
 
 while getopts "V:uetvph" c
 do
         case $c in
         V)      VERSION="$OPTARG"
 		 ;;
+	o)	ONLINE="yes"
+		;;
 	u)	UPDATE="yes"
 		;;
 	e)	EDUCATION="yes"
@@ -36,6 +41,60 @@ do
                 ;;
         esac
 done
+
+if [ "${ONLINE}" == "yes" ]; then
+cp /srv/ftp/akt/xml/classes/common/addons.xml  /srv/ftp/akt/xml/classes/common/addons.xml.$DATE
+echo > /srv/ftp/akt/xml/classes/common/addons.xml <<EOF
+<?xml version="1.0"?>
+<!DOCTYPE profile>
+<profile xmlns="http://www.suse.com/1.0/yast2ns" xmlns:config="http://www.suse.com/1.0/configns">
+<add-on>
+  <add_on_products config:type="list">
+    <listentry>
+      <media_url>http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Leap_${VERSION}/</media_url>
+      <product>PACKMAN</product>
+      <product_dir>/</product_dir>
+      <ask_on_error config:type="boolean">false</ask_on_error>
+      <name>PACKMAN</name>
+      <priority config:type="integer">10</priority>
+    </listentry>
+    <listentry>
+      <media_url>http://download.opensuse.org/distribution/leap/${VERSION}/repo/oss/</media_url>
+      <product>openSUSE-${VERSION}</product>
+      <product_dir>/</product_dir>
+      <ask_on_error config:type="boolean">false</ask_on_error>
+      <name>openSUSE-${VERSION}</name>
+      <priority config:type="integer">20</priority>
+    </listentry>
+    <listentry>
+      <media_url>http://download.opensuse.org/update/leap/${VERSION}/oss/</media_url>
+      <product>openSUSE-Updates</product>
+      <product_dir>/</product_dir>
+      <ask_on_error config:type="boolean">false</ask_on_error>
+      <name>openSUSE-Updates</name>
+      <priority config:type="integer">20</priority>
+    </listentry>
+    <listentry>
+      <media_url>http://download.opensuse.org/update/leap/${VERSION}/sle/</media_url>
+      <product>SLE15-Updates</product>
+      <product_dir>/</product_dir>
+      <ask_on_error config:type="boolean">false</ask_on_error>
+      <name>SLE15-Updates</name>
+      <priority config:type="integer">20</priority>
+    </listentry>
+    <listentry>
+      <media_url>ftp://install/akt/education</media_url>
+      <product>openSUSE-Education</product>
+      <product_dir>/</product_dir>
+      <ask_on_error config:type="boolean">false</ask_on_error>
+      <name>openSUSE-Education</name>
+      <priority config:type="integer">20</priority>
+    </listentry>
+  </add_on_products>
+</add-on>
+</profile>
+EOF
+fi
 
 mkdir -p /srv/ftp/akt/{CD1,xml,non-oss,OSS,updates,updates-non-oss}
 if [ -e /srv/ftp/akt/RC1/ ]; then
